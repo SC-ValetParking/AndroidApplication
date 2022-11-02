6288dd93ad2ad10d139c71656ace78851e8b4f50
a101a78 <36858137+a101a78@users.noreply.github.com>
2022-11-02 22:39:41 +0900

    Feat: Code update
    Feat: generalArea 매커니즘 변경, specialArea 코드 작성.

--- app/src/main/java/com/competition/valetparking/MainActivity.kt
+++ app/src/main/java/com/competition/valetparking/MainActivity.kt
@@ -7,11 +7,7 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
-import android.view.ViewGroup
-import android.widget.ProgressBar
-import android.widget.TableLayout
-import android.widget.TableRow
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentManager
@@ -19,7 +15,17 @@ import com.naver.maps.map.*
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    enum class SpecialType(val type: Int) {
+        LIGHT(1), DISABLED(2)
+    }
+
+    data class ParkingData(
+        var specialType: SpecialType,
+        var using: Boolean
+    )
+
     private lateinit var mNaverMap: NaverMap
+    private val parkingSize = 20
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,34 +39,27 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         NaverMapSdk.getInstance(this).client =
             NaverMapSdk.NaverCloudPlatformClient(BuildConfig.NAVERMAP_CLIENT_ID)
 
-        val parkingLayout: TableLayout = findViewById(R.id.parking_layout)
-        var tableRow = TableRow(this)
-        tableRow.layoutParams = TableRow.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-
-        val parkingMap = HashMap<Int, Boolean>()
-        parkingMap[15] = true
-        parkingMap[8] = true
-        parkingMap[1] = true
-        parkingMap[4] = true
-        parkingMap[5] = true
-        parkingMap[3] = true
-        parkingMap[17] = true
-        parkingMap[14] = true
-        parkingMap[6] = true
-        parkingMap[19] = true
-        parkingMap[11] = true
-        parkingMap[13] = true
-        /*parkingMap[16] = true
-        parkingMap[9] = true
-        parkingMap[10] = true
-        parkingMap[0] = true
-        parkingMap[7] = true*/
+        val parkingMap = HashMap<Int, ParkingData?>() //number, ParkingData{ specialType, using }
+        parkingMap[1] = null
+        parkingMap[3] = null
+        parkingMap[4] = null
+        parkingMap[5] = ParkingData(SpecialType.DISABLED, false)
+        parkingMap[6] = null
+        parkingMap[7] = null
+        parkingMap[8] = ParkingData(SpecialType.DISABLED, false)
+        parkingMap[9] = null
+        parkingMap[11] = ParkingData(SpecialType.LIGHT, false)
+        parkingMap[12] = null
+        parkingMap[13] = null
+        parkingMap[14] = null
+        parkingMap[17] = null
+        parkingMap[19] = null
+        parkingMap[20] = ParkingData(SpecialType.LIGHT, true)
 
         val saturationBar: ProgressBar = findViewById(R.id.saturation_bar)
-        val progress: Int = (parkingMap.size * 100) / 20
+        val progress: Int = (parkingMap.filterValues { it == null }.size * 100) / parkingSize
         saturationBar.progress = progress
+        Log.d(localClassName, "progress: $progress")
         val color: Int = when (progress) {
             in 1..25 -> R.color.green
             in 26..50 -> R.color.yellow
@@ -68,29 +67,66 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             else -> R.color.red
         }
         saturationBar.progressTintList = ColorStateList.valueOf(ContextCompat.getColor(this, color))
-        Log.d(localClassName, "progress: " + saturationBar.progress)
 
-        for (i in 0 until 20) {
-            val textView = TextView(this)
-            textView.layoutParams = TableRow.LayoutParams(50, 110)
-            textView.text = String.format("%02d", i + 1)
-            textView.textSize = 16F
-            textView.typeface = Typeface.create(textView.typeface, Typeface.BOLD)
-            textView.setTextColor(Color.DKGRAY)
-            textView.gravity = Gravity.CENTER
+        val generalArea: TableLayout = findViewById(R.id.general_parking_layout)
+        val specialArea: TableLayout = findViewById(R.id.special_parking_layout)
+        val specialList = ArrayList<Int>()
+        var generalChild = TableRow(this)
+
+        var peek = 0
+        for (i in 1..parkingSize) {
+            if (parkingMap[i] != null) {
+                specialList.add(i)
+                continue
+            }
+            val textView = newLocText()
+            textView.text = String.format("%02d", i)
             textView.background = ColorDrawable(
                 ContextCompat.getColor(
-                    this, if (parkingMap[i] == true) {
+                    this, if (parkingMap.containsKey(i)) {
                         R.color.using
                     } else {
                         R.color.free
                     }
                 )
             )
-            tableRow.addView(textView)
-            if ((i + 1) % 4 == 0) {
-                parkingLayout.addView(tableRow)
-                tableRow = TableRow(this)
+            generalChild.addView(textView)
+            peek++
+            if (peek % 4 == 0 || i == parkingSize) {
+                generalArea.addView(generalChild)
+                generalChild = TableRow(this)
+            }
+        }
+
+        var specialChild = TableRow(this)
+        for (i in 0 until specialList.size) {
+            val value = specialList[i]
+            val linearLayout = LinearLayout(this)
+            val imageView = ImageView(this)
+            val textView = newLocText()
+            val image = when (parkingMap[value]!!.specialType) {
+                SpecialType.LIGHT -> R.drawable.ic_baseline_car
+                else -> R.drawable.ic_baseline_disabled
+            }
+            imageView.setImageResource(image)
+            textView.text = String.format("%02d", value)
+            linearLayout.orientation = LinearLayout.HORIZONTAL
+            linearLayout.gravity = Gravity.CENTER
+            linearLayout.background = ColorDrawable(
+                ContextCompat.getColor(
+                    this, if (parkingMap[value]!!.using) {
+                        R.color.using
+                    } else {
+                        R.color.free
+                    }
+                )
+            )
+            linearLayout.addView(imageView)
+            linearLayout.addView(textView)
+            specialChild.addView(linearLayout)
+            if ((i + 1) % 4 == 0 || i == specialList.size) {
+                specialArea.addView(specialChild)
+                specialChild = TableRow(this)
             }
         }
     }
@@ -99,5 +135,15 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mNaverMap = naverMap
         val uiSettings: UiSettings = mNaverMap.uiSettings
         uiSettings.isZoomControlEnabled = false
+    }
+
+    private fun newLocText(): TextView {
+        val locText = TextView(this)
+        locText.layoutParams = TableRow.LayoutParams(50, 110)
+        locText.textSize = 16F
+        locText.typeface = Typeface.create(locText.typeface, Typeface.BOLD)
+        locText.setTextColor(Color.DKGRAY)
+        locText.gravity = Gravity.CENTER
+        return locText
     }
 }
